9018ad775d550b6301a11f6726d4cc98ea4edca1
DJLGabJean <116765613+DJLGabJean@users.noreply.github.com>
2025-02-24 11:37:45 +0100

    Ajout d'une vraie page d'accueil + Modification du CSS + Ajout des contrôleurs de Client et Product + Ajout du fichier pour la vue de la liste des produits

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,11 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+final class ProductController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/product', name: 'product_list')]
     public function index(): Response
     {
-        return $this->render('index.html.twig');
+        return $this->render('product_view.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
     }
 }
