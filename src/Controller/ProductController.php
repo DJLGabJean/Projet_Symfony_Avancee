a7aee13b10a7638d0ffafc87500b05240b7e3dc4
DJLGabJean <116765613+DJLGabJean@users.noreply.github.com>
2025-02-25 09:04:27 +0100

    Ajout d'une vue sur la liste des produits

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,14 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ProductRepository;
 
 final class ProductController extends AbstractController
 {
     #[Route('/product', name: 'product_list')]
-    public function index(): Response
+    public function index(ProductRepository $productRepository): Response
     {
         return $this->render('product_view.html.twig', [
-            'controller_name' => 'ProductController',
+            'products' => $productRepository->findAll(),
         ]);
     }
 }
